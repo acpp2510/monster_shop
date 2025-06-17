d7a6adaa514113d7b366822e186f8bc2c18c5873
AnaCarinaPP <ancari1989@gmail.com>
2025-06-17 12:08:29 +0200

    chore(ProductRepository): change repository class name

--- src/main/java/com/api/monster_shop/repositories/ProductRepository.java
+++ src/main/java/com/api/monster_shop/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.api.monster_shop.repositories;
 
 import com.api.monster_shop.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-public interface MonsterShopRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
