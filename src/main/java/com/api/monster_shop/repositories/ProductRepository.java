faf59881b776ba12d6641da8d9b2b254291e2111
AnaCarinaPP <ancari1989@gmail.com>
2025-06-17 12:57:37 +0200

    feat(ProductService): add ProductService and getAllProducts, saveProduct methods

--- src/main/java/com/api/monster_shop/repositories/ProductRepository.java
+++ src/main/java/com/api/monster_shop/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.api.monster_shop.repositories;
 
 import com.api.monster_shop.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
