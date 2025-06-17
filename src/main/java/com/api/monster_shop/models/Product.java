fac9232910dbb8eafc4ce6514319e2ea84fa7f40
AnaCarinaPP <ancari1989@gmail.com>
2025-06-17 10:58:49 +0200

    feat(Product): create product entity with JPA annotations

---
+++ src/main/java/com/api/monster_shop/models/Product.java
@@ -0,0 +1,97 @@
+package com.api.monster_shop.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String name;
+
+    @Column (nullable = false)
+    private double price;
+
+    @Column (nullable = false)
+    private String imageUrl;
+
+    @Column (nullable = false)
+    private double rating;
+
+    @Column (nullable = false)
+    private int reviewCount;
+
+    @Column (nullable = false)
+    private boolean featured;
+
+    public Product() {
+    }
+
+    public Product(String name, double price, String imageUrl, double rating, int reviewCount, boolean featured) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.rating = rating;
+        this.reviewCount = reviewCount;
+        this.featured = featured;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getReviewCount() {
+        return reviewCount;
+    }
+
+    public void setReviewCount(int reviewCount) {
+        this.reviewCount = reviewCount;
+    }
+
+    public boolean isFeatured() {
+        return featured;
+    }
+
+    public void setFeatured(boolean featured) {
+        this.featured = featured;
+    }
+}
